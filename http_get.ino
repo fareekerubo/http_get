a31e6ba8ea3f131d534af7a7efd12fed63ae4e77
Farida Kerubo <kerubofarida@gmail.com>
2019-11-10 21:24:58 +0300

    Add files via upload

---
+++ http_get.ino
@@ -0,0 +1,87 @@
+#include <SoftwareSerial.h>
+SoftwareSerial SIM800(7,5); // RX, TX 
+
+
+void setup(){
+    SIM800.begin(9600);
+    Serial.begin(9600);
+    readHTTPRequest();
+    
+  
+}
+
+void loop()
+{
+  SIM800.println("AT+CSPN?");
+  delay(100);
+  readHTTPRequest();
+  
+  
+}
+
+
+void readHTTPRequest()
+{
+ 
+  SIM800.println("AT+CSQ"); // Signal quality check
+
+  delay(100);
+ 
+  ShowSerialData(); // this code is to show the data from gprs shield, in order to easily see the process of how the gprs shield submit a http request, and the following is for this purpose too.
+  
+  SIM800.println("AT+CGATT?"); //Attach or Detach from GPRS Support
+  delay(100);
+ 
+  ShowSerialData();
+  
+  SIM800.println("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"");//setting the SAPBR, the connection type is using gprs
+  delay(1000);
+ 
+  ShowSerialData();
+ 
+  SIM800.println("AT+SAPBR=3,1,\"APN\",\"Safaricom\"");//setting the APN, Access point name string
+  delay(4000);
+ 
+  ShowSerialData();
+ 
+  SIM800.println("AT+SAPBR=1,1");//setting the SAPBR
+  delay(2000);
+ 
+  ShowSerialData();
+ 
+  SIM800.println("AT+HTTPINIT"); //init the HTTP request
+ 
+  delay(2000); 
+ // ShowSerialData();
+
+   SIM800.print("AT+HTTPPARA=\"URL\",\"https://api.thingspeak.com/channels/904744/fields/1.json?api_key=T0SRJC33VDOYDPYM&results=2");// setting the httppara, the second parameter is the website you want to access
+//  SIM800.print(acc_no);
+  SIM800.println("\"");
+  delay(1000);
+  
+   
+ 
+ 
+  ShowSerialData();
+ 
+  SIM800.println("AT+HTTPACTION=0");//submit the request 
+  delay(5000);//the delay is very important, the delay time is base on the return from the website, if the return datas are very large, the time required longer.
+  //while(!SIM900.available());
+ 
+  ShowSerialData();
+ 
+  SIM800.println("AT+HTTPREAD");// read the data from the website you access
+  delay(300);
+  //changeLed();
+  ShowSerialData();
+ 
+  SIM800.println("");
+  delay(100);
+}
+
+
+void ShowSerialData()
+{
+  while(SIM800.available()!=0)
+    Serial.write(char (SIM800.read()));
+}
